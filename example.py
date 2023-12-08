a8e5178174b3244efa6c38f409a4bc4b9a83b227
anglerfish27 <113139200+anglerfish27@users.noreply.github.com>
2023-12-08 17:48:16 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,32 +1,29 @@
 try:
-    
     from Helper import *
     from time import sleep
 
-    fed = Helper()
-    fed.wifi_connect()
-    sleep(1)
-    fed.test_comms()
+    fed = Helper() # create an instance of the Helper class
+
+    #Remove any methods you don't want/need. This helper example has a list of the current methods that are functional.
+
+    fed.wifi_connect() #Connect to Wi-Fi
+
+    fed.test_comms() # Ensures you have network connectivity to the outside world.
     
-    print(f'Battery Voltage: {fed.get_battery_voltage()}V')
-    print(f'Checking USB Connection: {fed.get_vbus_present()}')
+    fed.led_on('red') #Turn on RGB LED to Red
+    fed.led_on('green') #Turn on RGB LED to green
+    fed.led_on('yellow') #Turn on RGB LED to yellow
+    fed.led_on('blue') #Turn on RGB LED to Blue
+    fed.led_off() #Turn off RGB LED
+    print(f'Current battery voltage is {fed.battery_voltage()} volts if not connected to USB') #Provides battery voltage if plugged in, otherwise it returns USB power.
+    print(f'USB connection status:{fed.usb_detect()}') #provides if USB is plugged in or not.
+    fed.set_rtc_ntp()  #Sets the RTC via NTP time and also provides that time.
     
-    fed.rtc()
+
     
-    print("testing colors wee rainbow")
-    fed.led_on("green")
-    sleep(2)
-    fed.led_on("yellow")
-    sleep(2)
-    fed.led_on("red")
-    sleep(2)
-    print("bye")
-    fed.led_off()
-
-
-
-
-
-except KeyboardInterrupt:
+except KeyboardInterrupt:  #if you interrupt your program with a control + c this will ensure the LED and Wi-Fi are turned off.
     print("Shutting it down.")
+    
+finally:
     fed.led_off()
+    fed.wifi_disconnect()
